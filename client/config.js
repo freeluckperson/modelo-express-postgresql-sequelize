c3022758e64499ad7250f7f0ec4c722df967538e
erickubuntu <gestionecesaria@gmail.com>
2024-01-04 21:51:56 -0400

    add root client

--- client/config.js
+++ client/config.js
@@ -3,4 +3,4 @@ const DB_PASSWORD = process.env.DB_PASSWORD || "postgres";
 const DB_DATABASE = process.env.DB_DATABASE || "testapi";
 const DB_HOST = process.env.DB_HOST || "localhost";
 
-module.exports = { DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_HOST };
+module.exports = { DB_USERNAME, DB_PASSWORD, DB_DIALECT, DB_DATABASE, DB_HOST };
