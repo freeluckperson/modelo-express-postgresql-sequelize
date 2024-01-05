3a15cc70f7aaefad92897c31c95e04df3272407e
erickubuntu <gestionecesaria@gmail.com>
2024-01-04 21:54:18 -0400

    abc

--- config.js
+++
@@ -1,6 +0,0 @@
-const DB_USERNAME = process.env.DB_USERNAME || "postgres";
-const DB_PASSWORD = process.env.DB_PASSWORD || "postgres";
-const DB_DATABASE = process.env.DB_DATABASE || "testapi";
-const DB_HOST = process.env.DB_HOST || "localhost";
-
-module.exports = { DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_HOST };
